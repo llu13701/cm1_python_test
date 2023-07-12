94f32fb003efb8715e7201959389e570f1587652
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-07-12 15:02:12 -0400

    Task-3 #Done

---
+++ Task3.py
@@ -0,0 +1,31 @@
+import os
+from langchain.llms import OpenAI
+from langchain import ConversationChain
+from langchain import PromptTemplate
+
+#Initializing my OpenAI key
+os.environ['OPENAI_API_KEY'] = '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'
+
+#Initializing the model that we will use. (text-davinci-003 corressponds to turbo-3.5-gpt)
+llm = OpenAI(model_name = "text-davinci-003")
+
+#Using a chain for remembering the texts
+conversation = ConversationChain(llm = llm, verbose = True)
+
+#Sample Texts
+output = conversation.predict(input = "Hello")
+print(output)
+
+# Using a promt tempelate to make jokes about users' name.
+prompt = PromptTemplate(
+ input_variables=["name"],
+ template="Write a joke about my name. {name}",
+)
+
+formatted = prompt.format(name = "My name is Rajat")
+output = conversation.predict(input = formatted)
+print(output)
+
+#To demonstrate that it remember's the texts
+output = conversation.predict(input = "What was my first text in this conversation")
+print(output)
